d83c16ab600e967f61da8f63789dbe44f412768d
mujahed85 <mujahed.mca@gmail.com>
2025-11-11 22:17:12 +0530

    V1.3 - Improved UI Design

--- Generate/my-todo-app01/src/Todo.js
+++ Generate/my-todo-app01/src/Todo.js
@@ -3,6 +3,8 @@ import React, { useState } from 'react';
 function Todo() {
   const [tasks, setTasks] = useState([]);
   const [newTask, setNewTask] = useState('');
+  const [editIndex, setEditIndex] = useState(null);
+  const [editText, setEditText] = useState('');
 
   const handleAddTask = () => {
     if (newTask.trim()) {
@@ -11,9 +13,30 @@ function Todo() {
     }
   };
 
+  const handleDelete = (index) => {
+    const updatedTasks = tasks.filter((_, i) => i !== index);
+    setTasks(updatedTasks);
+  };
+
+  const handleEdit = (index) => {
+    setEditIndex(index);
+    setEditText(tasks[index]);
+  };
+
+  const handleUpdate = () => {
+    if (editText.trim()) {
+      const updatedTasks = [...tasks];
+      updatedTasks[editIndex] = editText;
+      setTasks(updatedTasks);
+      setEditIndex(null);
+      setEditText('');
+    }
+  };
+
   return (
     <div className="todo">
       <h2>Todo List</h2>
+
       <input
         type="text"
         value={newTask}
@@ -21,10 +44,27 @@ function Todo() {
         placeholder="Add a new task"
       />
       <button onClick={handleAddTask}>Add Task</button>
-      
+
       <ul>
         {tasks.map((task, index) => (
-          <li key={index}>{task}</li>
+          <li key={index}>
+            {editIndex === index ? (
+              <>
+                <input
+                  type="text"
+                  value={editText}
+                  onChange={(e) => setEditText(e.target.value)}
+                />
+                <button onClick={handleUpdate}>Update</button>
+              </>
+            ) : (
+              <>
+                {task}
+                <button onClick={() => handleEdit(index)}>Edit</button>
+                <button onClick={() => handleDelete(index)}>Delete</button>
+              </>
+            )}
+          </li>
         ))}
       </ul>
     </div>
